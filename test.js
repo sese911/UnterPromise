e008d85222029ac5bc7bc863dfd7cbae838b1719
Sergey Serebryakov <sesecoder@gmail.com>
2020-03-24 14:46:03 +0600

    Added few class's state tests

--- test.js
+++ test.js
@@ -20,4 +20,38 @@ describe("Тесты конструктора класса UnterPromise", functi
 
         assert.equal(testValue, "foo");
     });
+
+    it("Конструктор передает экзекьютеру в качестве аргументов внутренние методы промиса, изменяющие сосотояние промиса и присваивающие переданный им аргумент результату промиса", function() {
+        let testValue = "foo";
+        let testError = new Error("Тестовая ошибка!");
+        let resolvedPromise = new UnterPromise(resolve => {
+            assert.typeOf(resolve, "function");
+            resolve(testValue);
+        });
+        let rejectedPromise = new UnterPromise((resolve, reject) => {
+            assert.typeOf(reject, "function");
+            reject(testError);
+        });
+        
+        assert.equal(resolvedPromise._status, "fulfilled");
+        assert.equal(resolvedPromise._result, testValue);
+        assert.equal(rejectedPromise._status, "rejected");
+        assert.equal(rejectedPromise._result, testError);
+    });
+
+    it("Статус промиса изменяется только один раз, последующие попытки разрешить или отклонить промис игнорирутся", function() {
+
+        let testValue_1 = "foo";
+        let testValue_2 = "bar";
+        let testError = new Error("Тестовая ошибка!");
+
+        let promise = new UnterPromise((resolve, reject) => {
+            resolve(testValue_1);
+            resolve(testValue_2);
+            reject(testError);
+        });
+
+        assert.equal(promise._result, testValue_1);
+        assert.equal(promise._status, "fulfilled");
+    });
 });
