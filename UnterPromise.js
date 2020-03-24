29263112bc833fc5438a3807ba5cbb1329f5a197
Sergey Serebryakov <sesecoder@gmail.com>
2020-03-24 14:47:23 +0600

    Implemented state change

--- UnterPromise.js
+++ UnterPromise.js
@@ -1,10 +1,29 @@
 class UnterPromise {
     constructor(executor) {
+        this._status = "pending";
+        this._result;
+
         if (typeof executor === "function") {
-            executor();
+            executor(value => this._resolve(value), error => this._reject(error));
         }
         else {
             throw TypeError(`UnterPromise executor ${typeof executor} is not a function`);
         }
     }
+
+    _resolve(value) {
+        if (this._status === "pending") {
+            this._status = "fulfilled";
+            this._result = value;
+        }
+    }
+
+    _reject(error) {
+        if (this._status === "pending") {
+            this._status = "rejected";
+            this._result = error;
+        }
+    }
+
+    
 }
