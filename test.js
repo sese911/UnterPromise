8a8d544916a1aa8a09395d13c592529614e15e05
Sergey Serebryakov <sesecoder@gmail.com>
2020-03-24 20:43:24 +0600

    Added test of .then methods chaining

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ describe("Тесты конструктора класса UnterPromise", functi
         let testError;
 
         try {
-            new UnterPromise()
+            new UnterPromise();
         }
         catch (error) {
             testError = error;
@@ -107,7 +107,7 @@ describe("Тесты метода .then", function() {
         }, 200);
     });
 
-    it("Метод .then принимает вторым аргументом функцию, которая должна быть вызвана при отклонении промиса с результатом промиса в качастве аргумента", function(done) {
+    it("Метод .then принимает вторым аргументом функцию, которая должна быть вызвана при отклонении промиса с результатом промиса в качестве аргумента", function(done) {
         let testError = new Error("Тестовая ошибка!");
 
         new UnterPromise((resolve, reject) => {
@@ -155,5 +155,212 @@ describe("Тесты метода .then", function() {
             });
         }, 200);
     });
-    
+
+    it("Метод .then возвращает новый промис, который разрешится когда будет вызван один из обработчиков полученных методом .then", function(done) {
+        let onResolveCalled = false;
+        let onRejectCalled  = false;
+
+        new UnterPromise(resolve => {
+            setTimeout(() => resolve(), 100);
+        })
+        .then(() => {})
+        .then(() => {
+            onResolveCalled = true;
+        });
+        
+        new UnterPromise((resolve, reject) => {
+            setTimeout(() => reject(), 200);
+        })
+        .then(null, () => {})
+        .then(() => {
+            onRejectCalled = true;
+
+            assert.isTrue(onResolveCalled);
+            assert.isTrue(onRejectCalled);
+            done();
+        });
+    });
+
+    it("Если обработчик, переданный методу .then, возвращает значение, то промис созданный методом .then разрешается с этим значением", function(done) {
+        let testValue = "foobar";
+
+        new UnterPromise(resolve => {
+            setTimeout(() => resolve("foo"), 100);
+        })
+        .then(value => {
+            return value + "bar";
+        })
+        .then(value => {
+            assert.equal(value, testValue);
+            done();
+        });
+    });
+
+    it("Если обработчик, переданный методу .then, возвращает undefined, то промис созданный методом .then разрешается со значением undefined", function(done) {
+        let testValue;
+
+        new UnterPromise(resolve => {
+            setTimeout(() => resolve("foo"), 100);
+        })
+        .then(() => {
+            // ничего не возвращает
+        })
+        .then(value => {
+            assert.equal(value, testValue);
+            done();
+        });
+    });
+
+    it("Если обработчик, переданный методу .then, выдает ошибку, то промис созданный методом .then отклоняется со значением выданной ошибки", function(done) {
+        let testError = Error("Тестовая ошибка!");
+        let result_1, result_2;
+
+        new UnterPromise(resolve => {
+            setTimeout(() => resolve(), 100);
+        })
+        .then(() => {
+            throw testError;
+        })
+        .then(null, error => {
+            result_1 = error;
+        });
+        
+        new UnterPromise((resolve, reject) => {
+            setTimeout(() => reject(), 100);
+        })
+        .then(null, () => {
+            throw testError;
+        })
+        .then(null, error => {
+            result_2 = error;
+
+            assert.equal(result_1, testError);
+            assert.equal(result_2, testError);
+            done();
+        });
+    });
+
+    it("Если обработчик, переданный методу .then, возвращает разрешенный промис, то промис созданный методом .then разрешается со значением промиса возвращенного обработчиком", function(done) {
+        let testValue = "foo";
+        let result_1, result_2;
+
+        new UnterPromise(resolve => {
+            setTimeout(() => resolve(), 100);
+        })
+        .then(() => {
+            return new UnterPromise(resolve => resolve(testValue));
+        })
+        .then(value => {
+            result_1 = value;
+        });
+
+        new UnterPromise((resolve, reject) => {
+            setTimeout(() => reject(), 100);
+        })
+        .then(null, () => {
+            return new UnterPromise(resolve => resolve(testValue));
+        })
+        .then(value => {
+            result_2 = value;
+
+            assert.equal(result_1, testValue);
+            assert.equal(result_2, testValue);
+            done();
+        });
+    });
+
+    it("Если обработчик, переданный методу .then, возвращает отклоненный промис, то промис созданный методом .then отклоняется со значением промиса возвращенного обработчиком", function(done) {
+        let testError = new Error("Тестовая ошибка!");
+        let result_1, result_2;
+
+        new UnterPromise(resolve => {
+            setTimeout(() => resolve(), 100);
+        })
+        .then(() => {
+            return new UnterPromise((resolve, reject) => reject(testError));
+        })
+        .then(null, error => {
+            result_1 = error;
+        });
+
+        new UnterPromise((resolve, reject) => {
+            setTimeout(() => reject(), 100);
+        })
+        .then(null, () => {
+            return new UnterPromise((resolve, reject) => reject(testError));
+        })
+        .then(null, error => {
+            result_2 = error;
+
+            assert.equal(result_1, testError);
+            assert.equal(result_2, testError);
+            done();
+        });
+    });
+
+    it("Если обработчик, переданный методу .then, возвращает промис в состоянии ожидания, то промис созданный методом .then разрешается со значением промиса возвращенного обработчиком как только тот разрешится", function(done) {
+        let testValue = "foo";
+        let result_1, result_2;
+
+        new UnterPromise(resolve => {
+            setTimeout(() => resolve(), 100);
+        })
+        .then(() => {
+            return new UnterPromise(resolve => {
+                setTimeout(() => resolve(testValue), 100);
+            });
+        })
+        .then(value => {
+            result_1 = value;
+        });
+
+        new UnterPromise((resolve, reject) => {
+            setTimeout(() => reject(), 100);
+        })
+        .then(null, () => {
+            return new UnterPromise(resolve => {
+                setTimeout(() => resolve(testValue), 100);
+            });
+        })
+        .then(value => {
+            result_2 = value;
+
+            assert.equal(result_1, testValue);
+            assert.equal(result_2, testValue);
+            done();
+        });
+    });
+
+    it("Если обработчик, переданный методу .then, возвращает промис в состоянии ожидания, то промис созданный методом .then отклоняется со значением промиса возвращенного обработчиком как только тот отклонится", function(done) {
+        let testError = new Error("Тестовая ошибка!");
+        let result_1, result_2;
+
+        new UnterPromise(resolve => {
+            setTimeout(() => resolve(), 100);
+        })
+        .then(() => {
+            return new UnterPromise((resolve, reject) => {
+                setTimeout(reject(testError), 100);
+            });
+        })
+        .then(null, error => {
+            result_1 = error;
+        });
+
+        new UnterPromise((resolve, reject) => {
+            setTimeout(() => reject(), 100);
+        })
+        .then(null, () => {
+            return new UnterPromise((resolve, reject) => {
+                setTimeout(reject(testError), 100);
+            });
+        })
+        .then(null, error => {
+            result_2 = error;
+
+            assert.equal(result_1, testError);
+            assert.equal(result_2, testError);
+            done();
+        });
+    });    
 });
